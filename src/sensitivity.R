e861e1cb2cfc93a8e0552e5a82ad091dce562e4e
Zsofia Koma <komazsofi@gmail.com>
2021-01-29 14:29:41 +0100

    modify sensitivity

--- src/sensitivity.R
+++ src/sensitivity.R
@@ -15,25 +15,16 @@ data_orig=rbind(data1,data2)
 
 # select attributes
 
-data_orig_sel=subset(data_orig, select=c("Transect","Transect_sec","nofret_pheightlay_b02","zmean","z090quantile.90.","var_nofret",
-                                         "pulsepen","zkurto","zkurto_b1","zsd","z025quantile.25.","z050quantile.50.","z075quantile.75.",
-                                         "shannon"))
+data_orig_sel=subset(data_orig, select=c("Transect","Transect_sec","nofret_pheightlay_b02","nofret_pheightlay_02_1","nofret_pheightlay_1_5","nofret_pheightlay_5_20","nofret_pheightlay_a20",
+                                         "z090quantile.90.","rough_dsm_med","rough_dsm_med_b1","dtm_slope_mean",))
 
 data_sel=subset(data, select=c("Transect","Transect_sec","nofret_pheightlay_b02","zmean","z090quantile.90.","var_nofret",
                                          "pulsepen","zkurto","zkurto_b1","zsd","z025quantile.25.","z050quantile.50.","z075quantile.75.",
                                          "shannon"))
 
-data_orig_selsel=subset(data_orig, select=c("Transect","Transect_sec","nofret_pheightlay_b02","z090quantile.90.","dtm_slope_mean"))
-
-data_selsel=subset(data, select=c("Transect","Transect_sec","nofret_pheightlay_b02","z090quantile.90.","dtm_slope_mean"))
-
-names(data_orig_selsel) <- c("Transect","Transect_sec","Below 0.2 m density","Height","Slope")
-names(data_selsel) <- c("Transect","Transect_sec","Below 0.2 m density mod","Height mod","Slope mod")
+names(data_orig_sel) <- c("Transect","Transect_sec","Below 0.2 m density","Height","Slope")
+names(data_sel) <- c("Transect","Transect_sec","Below 0.2 m density mod","Height mod","Slope mod")
 
 data_forsens=merge(x=data_orig_selsel,y=data_selsel, by=c("Transect","Transect_sec"))
 
-#corr
-
-tiff("corBplot.tiff", width = 6, height = 5, units = 'in', res = 600)
-ggcorr(data_forsens[,c(3:8)], c("pairwise", "spearman"), name = expression(italic("Spearman's r")), label=TRUE, label_alpha=TRUE, label_size=2.5, hjust=0.85, size=3, layout.exp=3)
-dev.off()
+# visualization
